248dc2364f00de8615ef48b133fd3bbc86a69942
jena.hajek <jena.hajek@gmail.com>
2019-05-18 23:17:01 +0200

    fix path to svg map

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import CanvasMap from './canvas-map'
 
 let map=CanvasMap({
   textContainer:document.querySelector('.text'),
-  mapSrc:'../svg/map-new.svg',
+  mapSrc:'svg/map-new.svg',
   trailVisitedColor:'#872671',
   fontPresentColor:'#872671',
 }).appendTo('.container')
