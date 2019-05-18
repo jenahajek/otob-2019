b1183748334381b5bd10478a9ad548954602b09d
jena.hajek <jena.hajek@gmail.com>
2019-05-18 23:27:58 +0200

    use url path temporarily to svg-map

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import CanvasMap from './canvas-map'
 
 let map=CanvasMap({
   textContainer:document.querySelector('.text'),
-  mapSrc:'svg/map-new.svg',
+  mapSrc:'http://jenahajek.com/otob-2019/svg/map-new.svg',
   trailVisitedColor:'#872671',
   fontPresentColor:'#872671',
 }).appendTo('.container')
